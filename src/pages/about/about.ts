fef9356a7b2b616934d0534bc76557d9613f4605
renardipp <renardipurnamaputra@gmail.com>
2018-05-21 02:59:46 +0700

    update pesanan

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+
+
 
 @Component({
   selector: 'page-about',
@@ -7,7 +9,11 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  pet: string = "puppies";
+    isAndroid: boolean = false;
+
+  constructor(public navCtrl: NavController, platform: Platform) {
+     this.isAndroid = platform.is('android');
 
   }
 
